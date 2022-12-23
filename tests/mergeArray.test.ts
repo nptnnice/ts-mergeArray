cac266a2a9c5f1a5a6ffa3e12097d6d9eba7d9ba
nptnnice <nptnnice29@gmail.com>
2022-12-23 22:45:10 +0700

    edit test case

--- tests/mergeArray.test.ts
+++ tests/mergeArray.test.ts
@@ -1,34 +1,25 @@
 import { MergeArray } from "../src/mergeArray";
 
-describe("MergeTwoDistinctSortedArray", () => {
-    it("should merge two arrays in ascending order", () => {
+describe("MergeArray", () => {
+    it("can merge two arrays in ascending order when the two inputs are sorted arrays", () => {
         const mergeArray = new MergeArray();
         const result = mergeArray.merge([-2, 2, 3, 5], [-1, 0, 4, 6]);
         expect(result).toEqual([-2, -1, 0, 2, 3, 4, 5, 6]);
     });
-}
-);
 
-describe("MergeDuplicatedArray", () => {
-    it("should have the same number for each two consecutive numbers", () => {
+    it("should have the same number for each two consecutive numbers when the two inputs are the same", () => {
         const mergeArray = new MergeArray();
         const result = mergeArray.merge([1, 2, 3, 4], [1, 2, 3, 4]);
         expect(result).toEqual([1, 1, 2, 2, 3, 3, 4, 4]);
     });
-}
-);
 
-describe("MergeOneArrayWithEmptyArray", () => {
-    it("should remain the array that is not empty", () => {
+    it("should remain the array that is not empty when one of the inputs is an empty array", () => {
         const mergeArray = new MergeArray();
         const result = mergeArray.merge([1, 2, 3, 4], []);
         expect(result).toEqual([1, 2, 3, 4]);
     });
-}
-);
 
-describe("MergeTwoEmptyArray", () => {
-    it("should remain empty", () => {
+    it("should remain empty when the two inputs are empty arrays", () => {
         const mergeArray = new MergeArray();
         const result = mergeArray.merge([], []);
         expect(result).toEqual([]);
